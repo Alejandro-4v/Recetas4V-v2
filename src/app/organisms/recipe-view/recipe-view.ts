4e77735e1861a6dd02b544edf1284bcba298a96f
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 22:57:58 +0100

    feat: Card para Recipe creada y testada. Toda la lógica para representar la card encapsulada en el organismo

--- src/app/organisms/recipe-view/recipe-view.ts
+++ src/app/organisms/recipe-view/recipe-view.ts
@@ -1,11 +1,21 @@
 import { Component, input, output } from '@angular/core';
+import { Recipe } from '@models/recipe';
+import { RatingStars } from '@molecules/rating-stars/rating-stars';
 
 @Component({
   selector: 'app-recipe-view',
-  imports: [],
+  imports: [RatingStars],
   templateUrl: './recipe-view.html',
   styleUrl: './recipe-view.scss',
 })
 export class RecipeView {
+
+  recipe = input.required<Recipe>();
+
+  eliminar = output<number>();
+
+  onEliminar() {
+    this.eliminar.emit(this.recipe().id);
+  }
   
 }
