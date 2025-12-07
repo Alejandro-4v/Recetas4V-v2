1eb2e1f3fb00f7e7b29f0368d5b51a356724af0c
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 13:46:22 +0100

    feat: Funcionalidad de RatingStars creada y testada para mostrar estrellas con puntuaciones del 0 al 5 junto a la cantidad de reseñas

--- src/app/molecules/rating-stars/rating-stars.ts
+++ src/app/molecules/rating-stars/rating-stars.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
+import { Star } from '@atoms/star/star';
 
 @Component({
   selector: 'app-rating-stars',
-  imports: [],
+  imports: [Star],
   templateUrl: './rating-stars.html',
   styleUrl: './rating-stars.scss',
 })
 export class RatingStars {
+
+  rating = input<number>(0);
+  reviews = input<number>(0);
+
+  fullStars = computed(() => Math.round(this.rating()));
+  ratingDecimals = computed(() => this.rating() - this.fullStars());
+
+  halfStars = computed(() => Math.round(this.ratingDecimals()));
+
+  emptyStars = computed(() => 5 - this.halfStars() - this.fullStars() );
 
 }
