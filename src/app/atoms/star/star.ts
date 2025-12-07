23e7cb0cf75fc3b26a1f8170e31e7e46c40759a7
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 12:05:21 +0100

    feat: Átomo de estrella completado con tres estilos (vacío, medio lleno y completo)

--- src/app/atoms/star/star.ts
+++ src/app/atoms/star/star.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-star',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './star.html',
   styleUrl: './star.scss',
 })
 export class Star {
+
+  type = input<'full' | 'half' | 'empty'>('empty');
+  size = input<number>(24);
 
 }
