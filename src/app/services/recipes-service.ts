515092376dd856bcac5c30ec98f0e44450d88fa3
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 18:27:32 +0100

    feat: Creado comportamiento para recuperar recetas de la API sin asociar sus Ratings, mapeando cada elemento por individual para que coincida con la interfaz de forma adecuada

--- src/app/services/recipes-service.ts
+++ src/app/services/recipes-service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { Recipe } from '@models/recipe';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesService {
   
+  constructor(private http: HttpClient) {}
+
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>('https://69302440778bbf9e007001bb.mockapi.io/recipe').pipe(
+      map(data => data.map(item => ({
+        id: Number(item.id),
+        recipeName: item.recipeName,
+        ingredients: item.ingredients || [],
+        prepTimeMinutes: item.prepTimeMinutes || 0,
+        ratings: item.ratings || [],
+        averageRating: item.averageRating || 0
+      })))
+    );
+  }
+
 }
