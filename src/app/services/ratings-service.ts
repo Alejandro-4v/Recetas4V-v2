59739d43563f1c3f1391b09b8d92b31dc54df1c8
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 18:36:00 +0100

    feat: Creado comportamiento para recuperar valoraciones (Ratings) de la API mapeando automáticamente cada campo del JSON con la interfaz

--- src/app/services/ratings-service.ts
+++ src/app/services/ratings-service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { Rating } from '@models/rating';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RatingsService {
-  
+
+  constructor(private http: HttpClient) { }
+
+  getRatings(): Observable<Rating[]> {
+    return this.http.get<Rating[]>(
+      'https://69302440778bbf9e007001bb.mockapi.io/rating'
+    );
+  }
+
 }
