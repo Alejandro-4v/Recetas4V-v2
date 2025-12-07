552c37e6ef0b7d9e79115ddc384fc6baecc3b6d0
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-07 21:54:29 +0100

    feat: Añadida la funcionalidad necesaria para obtener todas las recetas con el resto de campos de la interfaz, obteniéndolos y calculando en el propio getRecipes()

--- src/app/services/recipes-service.ts
+++ src/app/services/recipes-service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, forkJoin, map, switchMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { Recipe } from '@models/recipe';
+import { RatingsService } from '@services/ratings-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesService {
   
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private ratingsService: RatingsService) {}
 
   getRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>('https://69302440778bbf9e007001bb.mockapi.io/recipe').pipe(
-      map(data => data.map(item => ({
-        id: Number(item.id),
-        recipeName: item.recipeName,
-        ingredients: item.ingredients || [],
-        prepTimeMinutes: item.prepTimeMinutes || 0,
-        ratings: item.ratings || [],
-        averageRating: item.averageRating || 0
-      })))
+      switchMap(recipes => {
+        
+        const recipesWithRatings$ = recipes.map(recipe =>
+          this.ratingsService.getRatingsFromRecipeId(Number(recipe.id)).pipe(
+            map(ratings => ({
+              ...recipe,
+              ratings,
+              averageRating: ratings.length
+                ? ratings.reduce((sum, r) => sum + r.rating, 0) / ratings.length
+                : 0
+            }))
+          )
+        );
+        return forkJoin(recipesWithRatings$);
+      })
     );
   }
 
