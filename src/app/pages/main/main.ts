a485a37b890cadd2fa834d5f1fea8a4c8c85c720
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-08 19:16:14 +0100

    feat: Diseñada e implementada lógica en la página principal (main) para mostrar las recetas y poder valorarlas (todavía sin poder filtrar)

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
+import { Recipe } from '@models/recipe';
+import { RecipesService } from '@services/recipes-service';
+
+import { Rating } from '@models/rating';
+import { RatingsService } from '@services/ratings-service';
+
+import { RecipeView } from '@organisms/recipe-view/recipe-view';
+import { RecipesList } from "@organisms/recipes-list/recipes-list";
+import { NewRatingForm } from "@organisms/new-rating-form/new-rating-form";
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterOutlet, RecipeView, RecipesList, NewRatingForm],
   templateUrl: './main.html',
-  styleUrl: './main.scss',
+  styleUrl: './main.scss'
 })
 export class Main {
+  protected readonly title = signal('Recetas4V-v2');
 
+  constructor(private recipesService: RecipesService, private ratingsService: RatingsService) { };
+
+  recipes: Recipe[] = [];
+
+  recipes$!: Observable<Recipe[]>;
+
+  ratings: Rating[] = [];
+
+  private recipesSubscription: Subscription | null = null;
+
+  private ratingsSubscription: Subscription | null = null;
+
+  ngOnInit(): void {
+    this.recipes$ = this.recipesService.getRecipes();
+    this.recipes$.subscribe(res => this.recipes = res)
+
+    this.recipesSubscription = this.recipesService.changesOnRecipes.subscribe(
+      () => {
+        this.recipesService.getRecipes().subscribe(res => this.recipes = res);
+      }
+    )
+
+    this.ratingsSubscription = this.ratingsService.changesOnRecipes.subscribe(
+      () => {
+        this.ratingsService.getRatings().subscribe(res => this.ratings = res);
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.recipesSubscription?.unsubscribe();
+    this.ratingsSubscription?.unsubscribe();
+  }
+
+  eliminar(id: number): void {
+    this.recipesService.deleteRecipe(id);
+  }
+
+  private idAValorar: number = 0;
+
+  private modalInstance: any;
+
+  setIdAValorar(recipeId: number): void {
+    this.idAValorar = recipeId;
+
+    const modal = document.getElementById('ratingModal');
+    this.modalInstance = new bootstrap.Modal(modal);
+    this.modalInstance.show();
+  }
+
+  getIdAValorar(): number {
+    return this.idAValorar;
+  }
+
+  onValorar(rating: Rating): void {
+    this.ratingsService.addRating(rating);
+    this.modalInstance.hide();
+  }
 }
