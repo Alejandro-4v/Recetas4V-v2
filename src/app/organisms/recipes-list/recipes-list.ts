4059e03f5f9c2a2fbc45c16b2fadbcfcd60ae1d5
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-08 17:28:30 +0100

    feat: Organismo para el listado de recetas creado y testado con servicios

--- src/app/organisms/recipes-list/recipes-list.ts
+++ src/app/organisms/recipes-list/recipes-list.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Recipe } from '@models/recipe';
+import { RecipesService } from '@services/recipes-service';
+import { Observable, Subscription } from 'rxjs';
+
+import { RecipeView } from '@organisms/recipe-view/recipe-view';
 
 @Component({
   selector: 'app-recipes-list',
-  imports: [],
+  imports: [RecipeView],
   templateUrl: './recipes-list.html',
   styleUrl: './recipes-list.scss',
 })
 export class RecipesList {
+
+  recipes: Recipe[] = [];
+  
+  recipes$!: Observable<Recipe[]>;
+
+  private recipesSubscription: Subscription | null = null;
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit(): void {
+    this.recipes$ = this.recipesService.getRecipes();
+    this.recipes$.subscribe(res => this.recipes = res);
+
+    this.recipesSubscription = this.recipesService.changesOnRecipes.subscribe(
+      () => {
+        this.recipesService.getRecipes().subscribe(res => this.recipes = res);
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.recipesSubscription?.unsubscribe();
+  }
+
+  eliminar(recipe: Recipe): void {
+    this.recipesService.deleteRecipe(recipe.id);
+  }
 
 }
