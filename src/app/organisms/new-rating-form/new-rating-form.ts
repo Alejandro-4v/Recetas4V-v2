5c8874c41fa2d5796b686753984c72b8853a1245
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-08 17:29:17 +0100

    feat: Modal para evaluar una receta creado y configurado para devovler directamente un objeto Rating

--- src/app/organisms/new-rating-form/new-rating-form.ts
+++ src/app/organisms/new-rating-form/new-rating-form.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Rating } from '@models/rating';
+import { Recipe } from '@models/recipe';
+import { LabeledField } from "@molecules/labeled-field/labeled-field";
 
 @Component({
   selector: 'app-new-rating-form',
-  imports: [],
+  imports: [LabeledField],
   templateUrl: './new-rating-form.html',
   styleUrl: './new-rating-form.scss',
 })
 export class NewRatingForm {
+
+  recipe = input.required<Recipe>();
+
+  enviar = output<Rating>();
+
+  onEnviar(): void {
+    const ratingInput = document.getElementById('rating-i') as HTMLInputElement;
+    const raterInput = document.getElementById('raterName-i') as HTMLInputElement;
+
+    const ratingValue = Number(ratingInput.value);
+    const raterName = raterInput.value;
+
+    if (isNaN(ratingValue) || ratingValue < 0 || ratingValue > 5) {
+      alert("La puntuación debe estar entre 0 y 5");
+      return;
+    }
+
+    const newRating: Rating = {
+      id: 0,
+      recipeId: this.recipe().id,
+      raterName: raterName,
+      rating: ratingValue
+    };
+
+    this.enviar.emit(newRating);
+  }
 
 }
